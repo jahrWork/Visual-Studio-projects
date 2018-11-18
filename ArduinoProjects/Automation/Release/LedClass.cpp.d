199ba76fb88e5c56abde98bbba589355e3457e83
jahrWork <jahramos@gmail.com>
2018-11-18 15:13:44 +0100

    order and manual

---
+++ ArduinoProjects/Automation/Release/LedClass.cpp.d
@@ -0,0 +1,15 @@
+C:\Users\juan\AppData\Local\Temp\VMBuilds\Automation\themtto_atmega328p-IClk8MHz\Release\LedClass.cpp.o: \
+ C:\Users\juan\AppData\Local\Temp\VMBuilds\Automation\themtto_atmega328p-IClk8MHz\Release\LedClass.cpp \
+ C:\Users\juan\AppData\Local\Temp\VMBuilds\Automation\themtto_atmega328p-IClk8MHz\Release\LedClass.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h
---
+++ ArduinoProjects/HelloWorld/Debug/HelloWorld.cpp.d
@@ -0,0 +1,16 @@
+C:\Users\juan\AppData\Local\Temp\VMBuilds\HelloWorld\themtto_atmega328p-IClk8MHz\Debug\HelloWorld.cpp.o: \
+ C:\Users\juan\AppData\Local\Temp\VMBuilds\HelloWorld\themtto_atmega328p-IClk8MHz\Debug\HelloWorld.cpp \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
+ C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Enterprise\Common7\IDE\Extensions\pq0yofgn.yje\Micro\ Platforms\default\debuggers\VM_DBG/VM_DBG.h \
+ C:\Users\juan\Dropbox\Software\Conectto-1.5.8\hardware\arduino\avr\cores\arduino/Arduino.h
