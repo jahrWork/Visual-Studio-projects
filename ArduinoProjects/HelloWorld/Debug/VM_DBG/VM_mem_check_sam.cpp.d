043f808b00336f9b760334dafb45a2305d8b43e6
MglAnglRT <marapadotamarit@gmail.com>
2021-11-24 14:45:18 +0100

    Delete aux files from git_chapter

--- ArduinoProjects/HelloWorld/Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\juan\AppData\Local\Temp\VMBuilds\HelloWorld\themtto_atmega328p-IClk8MHz\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\2017\Enterprise\Common7\IDE\Extensions\pq0yofgn.yje\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
